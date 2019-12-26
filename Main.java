1177edf537c5f5e9d29448a1b0263428a03d0435
alvinli <alvin.li.j@gmail.com>
2019-12-26 17:38:00 -0500

    functional for most connected Julia sets

--- Main.java
+++ Main.java
@@ -7,21 +7,23 @@ public class Main
         public static void main(String[] args)
         {
                 //read in the imaginary num used as the constant c.
-                /*
+
                 double reC = Double.parseDouble(args[0]);
                 double imC = Double.parseDouble(args[1]);
-                */
-                Complex c = new Complex(.3, -.01);
 
-                drawMandelbrot();
+                Complex c = new Complex(reC, imC);
+
+                //drawMandelbrot();
+                drawJulia(c);
         }
 
         //Methods for checking if a point is in a Julia set for some Complex c and drawing it.
         public static int inJulia(Complex z, Complex c)
         {
+                Complex w = z;
                 for(int i = 1; i <= 10000; i++) //i is the number of iterations
                 {
-                        w = Complex.add(w.square(), w);
+                        w = Complex.add(w.square(), c);
                         if(w.mag() > 2) return i; //the number of iterations i corresponds to a certain color in the picture.
                 }
                 return -1;//the number is in the set, the pixel is colored black.
@@ -38,13 +40,14 @@ public class Main
                         for(double j = 0; j <= 1; j+=0.001953125)
                         {
                                 Complex z = new Complex(4*i-2, 4*j-2);
-                                if(inJulia(z) == -1)
+                                if(inJulia(z, c) == -1)
                                 {
                                         StdDraw.point(i, j); //plots i,j on the plane
                                         System.out.println(z);
                                 }
                         }
                 }
+                System.out.println("end");
         }
 
         //Methods for checking if a point is in the Mandelbrot set and drawing it.
